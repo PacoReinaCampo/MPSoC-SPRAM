f9e433241d65962ca222a4dd1581b6e370505e3e
PacoReinaCampo <frareicam@gmail.com>
2020-12-31 03:34:29 +0100

    new commit

--- rtl/verilog/bb/core/msp430_ram.sv
+++ rtl/verilog/bb/core/msp430_ram.sv
@@ -47,34 +47,33 @@
  */
 
 module msp430_ram #(
-  parameter ADDR_MSB   =  6,         // MSB of the address bus
-  parameter MEM_SIZE   =  256        // Memory size in bytes
+  parameter AW       =   6,  // Address bus
+  parameter DW       =  16,  // Data bus
+  parameter MEM_SIZE = 256   // Memory size in bytes
 )
   (
-    // OUTPUTs
-    output      [15:0] ram_dout,       // RAM data output
-
-    // INPUTs
-    input [ADDR_MSB:0] ram_addr,       // RAM address
-    input              ram_cen,        // RAM chip enable (low active)
     input              ram_clk,        // RAM clock
-    input       [15:0] ram_din,        // RAM data input
+
+    input     [AW-1:0] ram_addr,       // RAM address
+    output    [DW-1:0] ram_dout,       // RAM data output
+    input     [DW-1:0] ram_din,        // RAM data input
+    input              ram_cen,        // RAM chip enable (low active)
     input        [1:0] ram_wen         // RAM write enable (low active)
   );
 
   // RAM
   //============
 
-  reg         [15:0] mem [0:(MEM_SIZE/2)-1];
-  reg   [ADDR_MSB:0] ram_addr_reg;
+  reg [DW-1:0] mem [0:(MEM_SIZE/2)-1];
+  reg [AW-1:0] ram_addr_reg;
 
-  wire        [15:0] mem_val = mem[ram_addr];
+  wire [DW-1:0] mem_val = mem[ram_addr];
 
   always @(posedge ram_clk) begin
     if (~ram_cen & ram_addr<(MEM_SIZE/2)) begin
       if      (ram_wen==2'b00) mem[ram_addr] <= ram_din;
-      else if (ram_wen==2'b01) mem[ram_addr] <= {ram_din[15:8], mem_val[7:0]};
-      else if (ram_wen==2'b10) mem[ram_addr] <= {mem_val[15:8], ram_din[7:0]};
+      else if (ram_wen==2'b01) mem[ram_addr] <= {ram_din[DW-1:DW/2], mem_val[DW/2-1:0]};
+      else if (ram_wen==2'b10) mem[ram_addr] <= {mem_val[DW-1:DW/2], ram_din[DW/2-1:0]};
       ram_addr_reg <= ram_addr;
     end
   end
--- rtl/vhdl/bb/core/msp430_ram.vhd
+++ rtl/vhdl/bb/core/msp430_ram.vhd
@@ -47,25 +47,24 @@ use work.msp430_pkg.all;
 
 entity msp430_ram is
   generic (
-    ADDR_MSB : integer := 6;     -- MSB of the address bus
+    AW       : integer := 6;     -- Address bus
+    DW       : integer := 16;    -- Data bus
     MEM_SIZE : integer := 256);  -- Memory size in bytes
   port (
-    -- OUTPUTs
-    ram_dout : out std_logic_vector(15 downto 0);  -- RAM data output
+    ram_clk  : in std_logic;  -- RAM clock
 
-    -- INPUTs
-    ram_addr : in std_logic_vector(ADDR_MSB downto 0);  -- RAM address
-    ram_cen  : in std_logic;                            -- RAM chip enable (low active)
-    ram_clk  : in std_logic;                            -- RAM clock
-    ram_din  : in std_logic_vector(15 downto 0);        -- RAM data input
-    ram_wen  : in std_logic_vector(1 downto 0));        -- RAM write enable (low active)    
+    ram_addr : in std_logic_vector(AW-1 downto 0);   -- RAM address
+    ram_dout : out std_logic_vector(DW-1 downto 0);  -- RAM data output
+    ram_din  : in std_logic_vector(DW-1 downto 0);   -- RAM data input
+    ram_cen  : in std_logic;                         -- RAM chip enable (low active)
+    ram_wen  : in std_logic_vector(1 downto 0));     -- RAM write enable (low active)    
 end msp430_ram;
 
 architecture rtl of msp430_ram is
 
-  signal mem          : std_logic_matrix((MEM_SIZE/2)-1 downto 0)(15 downto 0);
-  signal ram_addr_reg : std_logic_vector(ADDR_MSB downto 0);
-  signal mem_val      : std_logic_vector(15 downto 0);
+  signal mem          : std_logic_matrix((MEM_SIZE/2)-1 downto 0)(DW-1 downto 0);
+  signal ram_addr_reg : std_logic_vector(AW-1 downto 0);
+  signal mem_val      : std_logic_vector(DW-1 downto 0);
 
 begin
   mem_val <= mem(to_integer(unsigned(ram_addr)));
@@ -77,9 +76,9 @@ begin
         if (ram_wen = "00") then
           mem(to_integer(unsigned(ram_addr))) <= ram_din;
         elsif (ram_wen = "01") then
-          mem(to_integer(unsigned(ram_addr))) <= (ram_din(15 downto 8) & mem_val(7 downto 0));
+          mem(to_integer(unsigned(ram_addr))) <= (ram_din(DW-1 downto DW/2) & mem_val(DW/2-1 downto 0));
         elsif (ram_wen = "10") then
-          mem(to_integer(unsigned(ram_addr))) <= (mem_val(15 downto 8) & ram_din(7 downto 0));
+          mem(to_integer(unsigned(ram_addr))) <= (mem_val(DW-1 downto DW/2) & ram_din(DW/2-1 downto 0));
         end if;
         ram_addr_reg <= ram_addr;
       end if;
