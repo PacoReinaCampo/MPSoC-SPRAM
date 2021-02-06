eb5427fbd78546a755a6d629277f366d7b385e55
PacoReinaCampo <frareicam@gmail.com>
2021-02-06 22:15:48 +0100

    Working in Peripheral-SPRAM

--- sim/verilog/tests/bfm/wb/iverilog/simulate.bat
+++ sim/verilog/tests/bfm/wb/iverilog/simulate.bat
@@ -1,4 +1,4 @@
 call ../../../../../../settings64_iverilog.bat
 
-iverilog -g2012 -o system.vvp -c system.vc -s mpsoc_spram_testbench -I ../../../../../../rtl/verilog/wb/pkg
+iverilog -g2012 -o system.vvp -c system.vc -s wb_bfm_tb -I ../../../../../../rtl/verilog/wb/pkg
 vvp system.vvp
