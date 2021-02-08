de91d95426505f5eca305e643db5b57f282f9192
PacoReinaCampo <frareicam@gmail.com>
2021-02-08 13:09:43 +0100

    Working in Peripheral-SPRAM

--- settings64_vivado.bat
+++ settings64_vivado.bat
@@ -3,4 +3,5 @@ REM #################################################################
 REM # Copyright (c) 1986-2020 Xilinx, Inc.  All rights reserved.    #
 REM #################################################################
 
-SET PATH=C:\Xilinx\Vivado\2020.1\bin;%PATH%
+SET PATH=C:\apps\Xilinx\Vivado\2018.2\bin;C:\apps\Xilinx\Vivado\2018.2\lib\win64.o;%PATH%
+SET XILINX_VIVADO=C:\apps\Xilinx\Vivado\2018.2
