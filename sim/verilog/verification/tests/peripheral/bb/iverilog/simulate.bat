02c76f22e8ac9909bae85c826a8c96fbf8941f74
PacoReinaCampo <frareicam@gmail.com>
2023-10-03 12:12:24 +0400

    Working in Peripheral-SPRAM 🔑

--- sim/verilog/verification/tests/peripheral/bb/iverilog/simulate.bat
+++ sim/verilog/verification/tests/peripheral/bb/iverilog/simulate.bat
@@ -45,6 +45,6 @@
 @echo off
 call ../../../../../../../settings64_iverilog.bat
 
-iverilog -g2012 -o system.vvp -c system.vc -s peripheral_spram_testbench
+iverilog -g2012 -o system.vvp -c system.s -s peripheral_spram_testbench
 vvp system.vvp
 pause
